7a34434cd465a77de2f2eea508a9353a5af88fd0
Alexander Sanchez Zamora <alexander.sanchezzamora@ucr.ac.cr>
2026-01-29 17:58:38 -0600

    Add react HolaMundo tutorial

--- React/HolaMundoTutorial/react-app/src/App.tsx
+++ React/HolaMundoTutorial/react-app/src/App.tsx
@@ -1,7 +1,24 @@
-import Titulo from "./titulo";
+import Card, { CardBody } from "./components/Card";
+import List from "./components/List";
 
 function App() {
-  return <Titulo></Titulo>;
+  const list = [
+    "Cras justo odio",
+    "Dapibus ac facilisis in",
+    "Morbi leo risus",
+    "Porta ac consectetur ac",
+    "Vestibulum at eros",
+  ];
+
+  const handleSelect = (elemento: string) => {
+    console.log("Elemento seleccionado:", elemento);
+  };
+  return (
+    <Card>
+      <CardBody title="Card Title" text="Card Text" />
+      <List data={list} onSelect={handleSelect} />
+    </Card>
+  );
 }
 
 export default App;
---
+++ React/HolaMundoTutorial/react-app/src/components/List.tsx
@@ -0,0 +1,30 @@
+import { MouseEvent, useState } from "react";
+
+type Props = {
+  data: string[];
+  onSelect?: (elemento: string) => void;
+};
+
+function List({ data, onSelect }: Props) {
+  const [index, setIndex] = useState(1);
+  const handleClick = (i: number, elemento: string) => {
+    setIndex(i);
+    onSelect?.(elemento); // Si onSelect es undefined, no se ejecuta
+  };
+
+  return (
+    <ul className="list-group">
+      {data.map((elemento, i) => (
+        <li
+          onClick={() => handleClick(i, elemento)}
+          key={elemento}
+          className={`list-group-item ${index == i ? "active" : ""}`}
+        >
+          {elemento}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default List;
