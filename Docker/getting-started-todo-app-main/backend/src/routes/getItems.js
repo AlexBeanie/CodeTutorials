b0ee16052bcc78af13b8d33c8c83e65cf9ab3b50
Alexander Sanchez Zamora <alexander.sanchezzamora@ucr.ac.cr>
2026-01-30 16:40:43 -0600

    change greeting to random list of greetins

--- Docker/getting-started-todo-app-main/backend/src/routes/getItems.js
+++ Docker/getting-started-todo-app-main/backend/src/routes/getItems.js
@@ -1,6 +1,11 @@
-const db = require('../persistence');
+const GREETINGS = [
+    "Whalecome!",
+    "All hands on deck!",
+    "Charting the course ahead!",
+];
 
 module.exports = async (req, res) => {
-    const items = await db.getItems();
-    res.send(items);
+    res.send({
+        greeting: GREETINGS[ Math.floor( Math.random() * GREETINGS.length )],
+    });
 };
